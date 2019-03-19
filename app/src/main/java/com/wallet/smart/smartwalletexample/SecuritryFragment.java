362abb30e85c2a3782845f84b5c76eae1c342e98
Michael Loney <michael.loney137@gmail.com>
2019-03-19 11:14:22 +0000

    Initial commit

---
+++ app/src/main/java/com/wallet/smart/smartwalletexample/SecuritryFragment.java
@@ -0,0 +1,100 @@
+package com.wallet.smart.smartwalletexample;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SecuritryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SecuritryFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private boolean securityState = true;
+
+    private ImageButton btnSecuritySwitch;
+
+    public SecuritryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SecuritryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SecuritryFragment newInstance(String param1, String param2) {
+        SecuritryFragment fragment = new SecuritryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_securitry, container, false);
+
+        try {
+            btnSecuritySwitch = view.findViewById(R.id.btnSecuritySwitch);
+
+            btnSecuritySwitch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(securityState){
+                        btnSecuritySwitch.setImageTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorGreen)));
+                        securityState = false;
+                    }else{
+                        btnSecuritySwitch.setImageTintList(ColorStateList.valueOf((getResources().getColor(R.color.colorRed))));
+                        securityState = true;
+                    }
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
